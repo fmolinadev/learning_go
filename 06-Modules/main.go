893c3b299f7316ac12ada58bd261596e7a977804
TheFranciscoMolina <fdm.molina@gmail.com>
2023-02-09 16:50:21 -0300

    add: modules examples and exercices

---
+++ 06-Modules/main.go
@@ -0,0 +1,22 @@
+package main
+
+//import "paquetes/figuras"
+
+import "github.com/donvito/hellomod"
+
+func main() {
+
+	hellomod.SayHello()
+
+	/**
+	p1 := models.Persona{}
+	p1.Constructor("Fran", 30)
+
+	fmt.Println(p1)
+	fmt.Println(p1.GetNombre())
+	p1.SetNombre("Moliii")
+
+	fmt.Println(p1)
+	*/
+
+}
---
+++ 06-Modules/messages/hello.go
@@ -0,0 +1,7 @@
+package messages
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello world!")
+}
